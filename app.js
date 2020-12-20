5e836669e8608171a09f18e4c35117c5cf42736f
siddharthPriyadarshi <74820758+siddharthPriyadarshi@users.noreply.github.com>
2020-12-20 21:33:17 +0530

    sith translator wired

--- app.js
+++ app.js
@@ -2,13 +2,35 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 
+// var serverUrl = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+
+var serverUrl = "https://api.funtranslations.com/translate/sith.json";
+
+function getTranslationUrl(text) {
+    return serverUrl + "?text= " + text ; 
+
+}
 
 
+function errorHandler(error) {
+    console.log("error occured",error);
+    alert("Something is wrong please try again later !")
+}
 
-console.log(outputDiv)
 
-function clickHandler() { 
-    outputDiv.innerText = "Sid"+txtInput.value;
+function clickHandler() {
+    // outputDiv.innerText = "Sid"+txtInput.value;
+
+    var inputText = txtInput.value; //taking input
+
+    //calling server for processing
+    fetch(getTranslationUrl(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText;
+        })
+        .catch(errorHandler)
 }
 
 btnTranslate.addEventListener("click", clickHandler);
